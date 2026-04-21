85860c26f901d7a75cbe1a6f3de7c7c91a5cf89b
Ander Criado Teijeira <and_cri_tei@goierrieskola.org>
2026-04-21 20:11:05 +0200

    eskatuordua.php hizkuntzaren aldaketa eginda

--- Lang/en.php
+++ Lang/en.php
@@ -8,5 +8,10 @@ return [
     "hasiera"      => "The beginning",
     "prezioak"     => "Prices",
     "eskatu_ordua" => "Request an appointment",
-    "saskia"       => "Basket"
+    "saskia"       => "Basket",
+    "zita_ondo" => "The appointment was successfully saved in the database!",
+    "denboraldia_etiketa" => "Day and Time (Season)",
+    "aukeratu_mota" => "Select the cleaning type...",
+    "auto_kalkulua" => "Automatically calculated",
+    "gorde_zita" => "Save Date",
 ];
--- Lang/eu.php
+++ Lang/eu.php
@@ -8,5 +8,10 @@ return [
     "hasiera"      => "Hasiera",
     "prezioak"     => "Prezioak",
     "eskatu_ordua" => "Eskatu ordua",
-    "saskia"       => "Saskia"
+    "saskia"       => "Saskia",
+    "zita_ondo" => "Zita ondo gorde da datu-basean!",
+    "denboraldia_etiketa" => "Eguna eta Ordua (Denboraldia)",
+    "aukeratu_mota" => "Aukeratu garbiketa mota...",
+    "auto_kalkulua" => "Automatiko kalkulatua",
+    "gorde_zita" => "Gorde Zita",
 ];
